041e7d07ab031d870cebef51401abe6cdccd43ea
LibertenA <112900190+LibertenA@users.noreply.github.com>
2023-12-08 16:26:59 +0300

    Delete Magazine UI/Magazine UI directory

--- Magazine UI/Magazine UI/src/TestProduct.java
+++
@@ -1,5 +0,0 @@
-public class TestProduct implements java.io.Serializable {
-    public static void main(String[] args) {
-        new Thread(() -> new ShowProductFrame().setVisible(true)).start();
-    }
-}
